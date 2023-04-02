841e8d337a2181dbd8eabf986a449ee30b2c3a1f
brodackasia <117479152+brodackasia@users.noreply.github.com>
2023-04-02 19:57:01 +0200

    Task #16 (#2)
    *controller, service, repository
    *dodanie danych do bazy danych
    *połączenie do bazy danych 
    *hasła env
    *zrobienie endpointa
    *zrobienie DTO

---
+++ src/Repository/CompanyRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Database\Connection;
+use App\DTO\CompanyDTO;
+use App\DTO\Factory\CompanyDTOFactory;
+use PDO;
+
+class CompanyRepository
+{
+    private Connection $db;
+
+    function __construct(Connection $db)
+    {
+        $this->db = $db;
+    }
+
+    public function getCompanyData(int $companyId): CompanyDTO
+    {
+        $statement = $this->db->prepare(<<<SQL
+            SELECT 
+                id, "name", vat_identification_number, address, city, zip_code
+            FROM 
+                company AS c
+            WHERE
+                c.id = :companyId
+        SQL);
+
+        $statement->execute([
+            'companyId' => $companyId
+        ]);
+
+        return CompanyDTOFactory::createFromArray(
+            $statement->fetch(PDO::FETCH_ASSOC)
+        );
+    }
+}
---
+++ src/Service/CompanyService.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\DTO\CompanyDTO;
+use App\Repository\CompanyRepository;
+
+class CompanyService
+{
+    private CompanyRepository $companyRepository;
+
+    function __construct(CompanyRepository $companyRepository)
+    {
+        $this->companyRepository = $companyRepository;
+    }
+
+    public function getCompany(int $companyId): CompanyDTO
+    {
+        return $this->companyRepository->getCompanyData($companyId);
+    }
+}
