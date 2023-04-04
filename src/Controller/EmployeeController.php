b938cb52e85cdb4fe1049a5aa80ce661732605f3
brodackasia <117479152+brodackasia@users.noreply.github.com>
2023-04-04 17:03:47 +0200

    task#28 (#6)
    * task#28

---
+++ src/Controller/EmployeeController.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Service\EmployeeService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EmployeeController extends AbstractController
+{
+    private EmployeeService $employeeService;
+
+    public function __construct(EmployeeService $employeeService)
+    {
+        $this->employeeService = $employeeService;
+    }
+
+    #[Route('/employee/{employeeId}', name:'employee', methods: 'GET')]
+    public function getEmployee(int $employeeId): JsonResponse
+    {
+        return new JsonResponse(
+            $this->employeeService->getEmployee($employeeId)
+        );
+    }
+
+}
