bcd44e9c9aa1f3a079c6a093404d380998407975
brodackasia <katarzynabrodacka1lo@gmail.com>
2023-05-06 15:43:00 +0200

    task#105

--- src/Command/CreateCompanyCommand.php
+++ src/Command/CreateCompanyCommand.php
@@ -4,39 +4,14 @@ declare(strict_types=1);
 
 namespace App\Command;
 
-class CreateCompanyCommand
+readonly class CreateCompanyCommand
 {
     public function __construct(
-        private readonly string $name,
-        private readonly string $vatIdentificationNumber,
-        private readonly string $address,
-        private readonly string $city,
-        private readonly string $zipCode
+        public string $name,
+        public string $vatIdentificationNumber,
+        public string $address,
+        public string $city,
+        public string $zipCode
     ) {
-    }
-
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    public function getVatIdentificationNumber(): string
-    {
-        return $this->vatIdentificationNumber;
-    }
-
-    public function getAddress(): string
-    {
-        return $this->address;
-    }
-
-    public function getCity(): string
-    {
-        return $this->city;
-    }
-
-    public function getZipCode(): string
-    {
-        return $this->zipCode;
     }
 }
